efc2ee9e6c5686c3c83effe5d3ecbb020210bf7c
= <>
2024-10-04 04:42:26 +0200

    bug fix on structure

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js'
+import ClassRoom from './0-classroom.js';
 
-axport default function initializeRooms(){
+export default function initializeRooms(){
 	const sizes = [9,20,34];
-	return sizes.map((el: number) => new ClassRoom(el));
+	return sizes.map((el) => new ClassRoom(el));
 }
